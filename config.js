2aaea8a9e65eecb140effef65e2dbfebe1856799
peiming <hyrijk@gmail.com>
2017-01-25 08:42:31 +0800

    添加赢面正式服配置

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 export default {
-    host: 'http://test2.eap.gzido.com:19090',
+    // host: 'http://test2.eap.gzido.com:19090',
+    // qiNiu: 'http://up-z2.qiniu.com/',
+    // qiNiuDomain: 'http://ojlisse1z.bkt.clouddn.com',
+
+    host: 'http://arhb.winpi.net',
     qiNiu: 'http://up-z2.qiniu.com/',
-    qiNiuDomain: 'http://ojlisse1z.bkt.clouddn.com',
+    qiNiuDomain: 'http://ok3vxa0yq.bkt.clouddn.com',
 }
